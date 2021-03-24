e3be466fddc54fa66830947902981a82c085b1c1
Kalsoom Begum <77676531+kb3gum@users.noreply.github.com>
2021-03-24 20:22:07 +0000

    inital commit

---
+++ Develop/script.js
@@ -0,0 +1,46 @@
+const startQuiz = []
+
+const startButton = document.getElementById("start-button");
+const timerSpanElement = document.getElementById("timer");
+const questionsContainer= document.getElementById("questions-container");
+
+> function createChoice(choices) { }
+
+const verifyChoice = (question-1) 
+  const target = question-1.;
+
+  if (target.matches ("button")) {
+    const answer = target.getAttribute ("data-answer")
+  }
+}
+
+// need to render each question 
+
+const questionsContainer = [
+  {
+    title: "Commonly used data types DO NOT INCLUDE:",
+    choices: ["Strings", "Booleans", "Alerts", "Numbers" ],
+    correctAnswer:"Alerts"
+
+    title: "The condition in an if an else statement is enclosed within:",
+    choices: ["Quotes", "Curly Braces", "Parenthesis", "Square Brackets" ],
+    correctAnswer:"Curly Braces"
+
+    title: "Arrays in Javascript can be used to store:",
+    choices: ["Numbers & Strings", "Other Arrays", "Bookmarks", "All of the above" ],
+    correctAnswer:"Curly Braces"
+
+    title: "String values must be enclosed within__________ when being assigned to variables:",
+    choices: ["Commas", "Curly Braces", "Parenthesis", "Quotes" ],
+    correctAnswer:"Curly Braces"
+
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["Javascript", "Terminal Bash", "for loops", "console,log" ],
+    correctAnswer:"Alerts"
+
+
+    
+
+
+  }
+]
