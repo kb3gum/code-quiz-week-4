5c6b911d31d640559c65204a35de1c7174b17873
Kalsoom Begum <77676531+kb3gum@users.noreply.github.com>
2021-03-28 22:20:16 +0100

    changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,163 @@
-const startQuiz = []
+// const startQuiz = []
 
 const startButton = document.getElementById("start-button");
+const nextButton = document.getElementById("next-button");
 const timerSpanElement = document.getElementById("timer");
-const questionsContainer= document.getElementById("questions-container");
+const questionsContainer = document.getElementById("questions-container");
+const introduction = document.getElementById("introduction");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const container = document.getElementById("container");
+let currentQuestionIndex = 0;
+var timer = 60;
 
-> function createChoice(choices) { }
-
-const verifyChoice = (question-1) 
-  const target = question-1.;
-
-  if (target.matches ("button")) {
-    const answer = target.getAttribute ("data-answer")
-  }
-}
-
-// need to render each question 
-
-const questionsContainer = [
+const questions = [
   {
     title: "Commonly used data types DO NOT INCLUDE:",
-    choices: ["Strings", "Booleans", "Alerts", "Numbers" ],
-    correctAnswer:"Alerts"
+    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correctAnswer: "Alerts",
+  },
 
+  {
     title: "The condition in an if an else statement is enclosed within:",
-    choices: ["Quotes", "Curly Braces", "Parenthesis", "Square Brackets" ],
-    correctAnswer:"Curly Braces"
+    choices: ["Quotes", "Curly Braces", "Parenthesis", "Square Brackets"],
+    correctAnswer: "Curly Braces",
+  },
 
+  {
     title: "Arrays in Javascript can be used to store:",
-    choices: ["Numbers & Strings", "Other Arrays", "Bookmarks", "All of the above" ],
-    correctAnswer:"Curly Braces"
+    choices: [
+      "Numbers & Strings",
+      "Other Arrays",
+      "Bookmarks",
+      "All of the above",
+    ],
+    correctAnswer: "Curly Braces",
+  },
 
-    title: "String values must be enclosed within__________ when being assigned to variables:",
-    choices: ["Commas", "Curly Braces", "Parenthesis", "Quotes" ],
-    correctAnswer:"Curly Braces"
+  {
+    title:
+      "String values must be enclosed within__________ when being assigned to variables:",
+    choices: ["Commas", "Curly Braces", "Parenthesis", "Quotes"],
+    correctAnswer: "Curly Braces",
+  },
 
-    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["Javascript", "Terminal Bash", "for loops", "console,log" ],
-    correctAnswer:"Alerts"
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["Javascript", "Terminal Bash", "for loops", "console,log"],
+    correctAnswer: "Alerts",
+  },
+];
 
+function startQuiz() {
+  introduction.classList.add("hide");
+  container.classList.remove("hide");
+  displayQuestion();
+  timerSpanElement.innerHTML = `Timer: ${timer}`;
+  setInterval(function () {
+    timerSpanElement.innerHTML = `Timer: ${timer--}`;
+  }, 1000);
+}
 
-    
+startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  displayQuestion();
+});
 
-
+function displayQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+  document.getElementById("questions").innerHTML = currentQuestion.title;
+  var answers = currentQuestion.choices;
+  var buttonGrid = document.createElement("div");
+  for (let index = 0; index < answers.length; index++) {
+    const element = answers[index];
+    const button = document.createElement("button");
+    button.classList.add("button");
+    button.innerText = element;
+    buttonGrid.appendChild(button);
   }
-]
+  answerButtonsElement.appendChild(buttonGrid);
+}
+
+// function showQuestion(question) {
+//   questionElement.innerText = question.quq;
+//   question.answers.forEach((answer) => {
+//     const button = document.createElement("button");
+//     button.classList.add("button");
+//     if (answer.correct) {
+//       button.dataset.correct = answer.correct;
+//     }
+//     button.addEventListener("click", selectAnswer);
+//     answer.ButtonsElement.appendChild(button);
+//   });
+// }
+
+// function resetState() {
+//   next.Button.classList.add("hide");
+//   while (answerButtonsElement.firstChild) {
+//     answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+//   }
+// }
+
+// function selectAnswer(e) {
+//   const selectedButton = e.target;
+//   const correct = selectedButton.dataset.correct;
+//   setStatusClass(document.body, correct);
+//   Array.from(answerButtonsElement.children).forEach((button) => {
+//     setStatusClass(button.dataset.correct);
+//   });
+//   next.Button.classList.remove("hide");
+// }
+
+// function setStatusClass(element, correct) {
+//   clearStatusClass(element);
+//   if (correct) {
+//     element.classList.add("correct");
+//   } else {
+//     element.classList.add("incorrect");
+//   }
+// }
+
+// function clearStatusClass(element) {
+//   element.classList.remove("correct");
+//   element.classList.remove("incorrect");
+// }
+
+// > function createChoice(choices) { }
+
+// const verifyChoice = (question-1)
+//   const target = question-1.;
+
+//   if (target.matches ("button")) {
+//     const answer = target.getAttribute ("data-answer")
+//   }
+// }
+
+// // need to render each question
+
+// const questions = [
+//   {
+//     title: "Commonly used data types DO NOT INCLUDE:",
+//     choices: ["Strings", "Booleans", "Alerts", "Numbers" ],
+//     correctAnswer:"Alerts"
+
+//     title: "The condition in an if an else statement is enclosed within:",
+//     choices: ["Quotes", "Curly Braces", "Parenthesis", "Square Brackets" ],
+//     correctAnswer:"Curly Braces"
+
+//     title: "Arrays in Javascript can be used to store:",
+//     choices: ["Numbers & Strings", "Other Arrays", "Bookmarks", "All of the above" ],
+//     correctAnswer:"Curly Braces"
+
+//     title: "String values must be enclosed within__________ when being assigned to variables:",
+//     choices: ["Commas", "Curly Braces", "Parenthesis", "Quotes" ],
+//     correctAnswer:"Curly Braces"
+
+//     title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+//     choices: ["Javascript", "Terminal Bash", "for loops", "console,log" ],
+//     correctAnswer:"Alerts"
+
+//   }
+// ]
---
+++ README.md
@@ -0,0 +1,15 @@
+## Description
+
+This is a quiz created by myself as a Bootcamp student to demonstrate some of my Javascript skills I have learnt on the course so far.
+
+This is an application to complete a coding assessment of multiple choice questions and interactive coding challenges.
+
+## Getting Started
+
+User will be presented with a landing page informing them about the quiz and the quiz requirements. User will need to click "Start Quiz" button to start the quiz. Upon clicking the button a countdown timer will start to record the time taken to complete the quiz. User will be taken through a set of questions however is the answer is incorrect the countdown timer will decrease by 10 seconds for each incorrect answer. If user does not complete the quiz in time the game will finish. If game finishes user will not be able to save initials. If user has successfully completed the game they will have to enter their initials for the score to be saved against.
+
+## Screenshots
+
+## Deployment Link
+
+Click [here] to view project on GitHub pages.
